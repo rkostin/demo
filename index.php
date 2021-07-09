eaa1f16f29cbd2b70d5719da7bc0893aed2cea4d
Robert M Kostin <3536295+rkostin@users.noreply.github.com>
2021-07-09 11:59:21 -0400

    Fixed typos

--- index.php
+++ index.php
@@ -7,7 +7,9 @@ include "inc/top-part.php";
 
 		<h1><?php echo $pageName; ?></h1>
 
-		<p>I am a professor of web design and development at the University of Rochester.  Before that I was an adjunct instructor at places like: the Rochester Institute of Technology (RIT); Monroe Community College (MCC); and a few other local colleges.  Before that I made my living as a contract consultant in the Update New York area, working at companies like Kodak, First Niagara Bank, Cornell University, the UR Medical Center, and several others.  And before that I had an 18-year career at as a marketing executive at Xerox.</p>
+		<h2>blah blah blah</h2>
+
+		<p>I am a professor of web design and development at the University of Rochester.  Before that I was an adjunct instructor at places like: the Rochester Institute of Technology (RIT); Monroe Community College (MCC); and a few other local colleges.  Before that I made my living as a contract consultant in the Update New York area, working at companies like Kodak, First Niagara Bank, Cornell University, the UR Medical Center, and several others.  And before that I had an 18-year career at as a marketing executive at Kodak.</p>
 
 	</main>
 	
